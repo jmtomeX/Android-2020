23eb7f7dc437e21aede7407133c04b375fccf518
jmtomeX <iremti2@gmail.com>
2020-02-03 18:59:39 +0100

    Adivina número evolućión 2

---
+++ AdivinaNumero/app/src/main/java/com/example/adivinanumero/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.adivinanumero;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText boxEditEmail;
+    EditText boxEditPassword;
+    Button mbtnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+         boxEditEmail = findViewById(R.id.boxtxtEmail);
+         boxEditPassword = findViewById(R.id.boxtxtPassword);
+         mbtnLogin = findViewById(R.id.btnLogin);
+        final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        final String email_admin = "iremti2@gmail.com";
+        final String passw_admin = "11111111";
+
+        mbtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = boxEditEmail.getText().toString();
+                String password = boxEditPassword.getText().toString();
+                if (email.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Inserta una dirección de correo", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (email.trim().matches(emailPattern)) {
+                        if (email.equals(email_admin) && password.equals(passw_admin)) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            //Pasar la var email entre activitis
+                            intent.putExtra("email", email);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Inserta una dirección de correo válida", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
--- AdivinaNumero/app/src/main/java/com/example/adivinanumero/MainActivity.java
+++ AdivinaNumero/app/src/main/java/com/example/adivinanumero/MainActivity.java
@@ -2,40 +2,57 @@ package com.example.adivinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int numeroMachine = (int) (Math.random() * 3) + 1;
+    int numeroMachine = (int) (Math.random() * 5) + 1;
+    int cont = 0;
+    // contador
+    TextView boxCont;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Recogemos las variables que nos puedan enviar:
+        String email = getIntent().getExtras().getString("email");
         Button mbtnPlay = findViewById(R.id.btnPlay);
-       final EditText minputNumber = findViewById(R.id.inputNumber);
+
+        boxCont = findViewById(R.id.boxTxtPlays);
+        TextView resEmail = findViewById(R.id.boxTxtRes);
+        //resEmail.setText("Bienvenido "+ email );
+        resEmail.setText(String.format("Bienvenido %s", email));
+
+        final EditText minputNumber = findViewById(R.id.inputNumber);
 
         mbtnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("MainActivity",""+numeroMachine);
+                cont++;
+                boxCont.setText(String.format("Número de intentos %s", cont));
+                Log.e("MainActivity", "" + numeroMachine);
                 String textoNum = minputNumber.getText().toString();
 
                 int numberUser = Integer.parseInt(textoNum);
                 String msg = "";
-                if(numberUser == numeroMachine) {
+                if (numberUser == numeroMachine) {
                     msg = "Has acertado el número";
                     numeroMachine = (int) (Math.random() * 3) + 1;
-                    Log.e("MainActivity","" + numeroMachine);
+                    Log.e("MainActivity", "" + numeroMachine);
+                    cont = 0;
                 } else {
                     msg = "No has acertado el número";
                 }
 
                 Toast.makeText(MainActivity.this,
-                        String.format("Respuesta %s",msg), Toast.LENGTH_SHORT).show();
+                        String.format("Respuesta %s", msg), Toast.LENGTH_SHORT).show();
             }
         });
 
